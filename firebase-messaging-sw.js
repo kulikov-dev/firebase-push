1c86b433ccc998f6ecefab8b42872515d9ba789b
kulikov-dev <kulikov.dev@gmail.com>
2022-10-13 11:56:13 +0600

    First commit

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,49 @@
+
+ // Give the service worker access to Firebase Messaging.
+ importScripts('https://www.gstatic.com/firebasejs/9.11.0/firebase-app-compat.js');
+ importScripts('https://www.gstatic.com/firebasejs/9.11.0/firebase-messaging-compat.js');
+
+ // Web app's Firebase configuration
+ const firebaseConfig = {
+   apiKey: "AIzaSyD4kj2g3e_SOQTmzBUtDn2_vx6UvcdWiG8",
+   authDomain: "kulikov-dev.firebaseapp.com",
+   projectId: "kulikov-dev",
+   storageBucket: "kulikov-dev.appspot.com",
+   messagingSenderId: "441130914859",
+   appId: "1:441130914859:web:2cb9c5736e4a595af0066e"
+ };
+
+ firebase.initializeApp(firebaseConfig);
+
+ // Retrieve an instance of Firebase Messaging so that it can handle background messages.
+ const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+
+  // Copy data object to get parameters in the click handler
+  payload.data.data = JSON.parse(JSON.stringify(payload.data));
+
+  return self.registration.showNotification(payload.data.title, payload.data);
+});
+
+ self.addEventListener('notificationclick', function(event) {
+     console.log('test', payload);
+   const target = event.notification.data.click_action || '/';
+   event.notification.close();
+
+   event.waitUntil(clients.matchAll({
+     type: 'window',
+     includeUncontrolled: true
+   }).then(function(clientList) {
+     // clientList always is empty?!
+     for (var i = 0; i < clientList.length; i++) {
+       var client = clientList[i];
+       if (client.url === target && 'focus' in client) {
+         return client.focus();
+       }
+     }
+
+     return clients.openWindow(target);
+   }));
+ });
---
+++ firebase_client.js
@@ -0,0 +1,137 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.11.0/firebase-app.js";
+import {getMessaging, getToken,onMessage } from "https://www.gstatic.com/firebasejs/9.11.0/firebase-messaging.js";
+
+// Web app's Firebase configuration
+// Should be copied during project initialization
+const firebaseConfig = {
+    apiKey: "AIzaSyD4kj2g3e_SOQTmzBUtDn2_vx6UvcdWiG8",
+    authDomain: "kulikov-dev.firebaseapp.com",
+    projectId: "kulikov-dev",
+    storageBucket: "kulikov-dev.appspot.com",
+    messagingSenderId: "441130914859",
+    appId: "1:441130914859:web:2cb9c5736e4a595af0066e"
+};
+
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
+// URL to webhook on your site for saving an user token.
+const webhook_url = 'YOUR_URL';
+const tokenSentTitle = 'tokenSentToServer';
+
+// Handle incoming messages. Called when:
+// - a message is received while the app has focus
+// - the user clicks on an app notification created by a service worker `messaging.onBackgroundMessage` handler.
+onMessage(messaging, (payload) => {
+    console.log('Message received. ', payload);
+
+    // TODO Update the UI if necessary to show the received message.
+});
+
+// Check if browser supports push-notifications
+if (
+    !('Notification' in window &&
+    'serviceWorker' in navigator &&
+    'localStorage' in window &&
+    'fetch' in window &&
+    'postMessage' in window)
+)
+{
+    if (!('Notification' in window)) {
+        console.error('Notification not supported');
+    } else if (!('serviceWorker' in navigator)) {
+        console.error('ServiceWorker not supported');
+    } else if (!('localStorage' in window)) {
+        console.error('LocalStorage not supported');
+    } else if (!('fetch' in window)) {
+        console.error('fetch not supported');
+    } else if (!('postMessage' in window)) {
+        console.error('postMessage not supported');
+    }
+
+    console.warn('This browser does not support push-notifications.');
+    console.log('Is HTTPS', window.location.protocol === 'https:');
+    console.log('Support Notification', 'Notification' in window);
+    console.log('Support ServiceWorker', 'serviceWorker' in navigator);
+    console.log('Support LocalStorage', 'localStorage' in window);
+    console.log('Support fetch', 'fetch' in window);
+    console.log('Support postMessage', 'postMessage' in window);
+} else {
+    if (Notification.permission === 'granted') {
+         getUserToken();
+    } else {
+        requestPermission();
+    }
+}
+
+// Request user permission for push-notifications
+function requestPermission() {
+    console.log('Requesting permission...');
+    Notification.requestPermission().then((permission) => {
+        if (permission === 'granted') {
+            console.log('Notification permission granted.');
+            getUserToken();
+        } else {
+            console.log('Unable to get permission to notify.');
+        }
+    });
+}
+
+// Get user registration token
+function getUserToken() {
+    // Register a service worker to use script with github pages (As firebase required to store serviceWorker only in the root.
+    if ("serviceWorker" in navigator) {
+        navigator.serviceWorker
+            .register("/firebase-push/firebase-messaging-sw.js")
+            .then(function(registration) {
+                console.log("Registration successful, scope is:", registration.scope);
+                // Get registration token. Initially this makes a network call, once retrieved
+                // subsequent calls to getToken will return from cache.
+                getToken(messaging,{vapidKey: 'BKpTvOreKY6M4vca8Qy1GfLda9seP0BaWFnkFaGvstDRknLfwDuTRHNPS8te28IP1Imm9LvZm0Q3GJwz7NuCDQg', serviceWorkerRegistration: registration}).then((currentToken) => {
+                    if (currentToken) {
+                        sendTokenToServer(currentToken);
+                    } else {
+                        // Show permission request.
+                        console.log('No registration token available. Request permission to generate one.');
+
+                        setTokenSentToServer(false);
+                    }
+                }).catch((err) => {
+                    console.log('An error occurred while retrieving token. ', err);
+                    setTokenSentToServer(false);
+                });
+            })
+            .catch(function(err) {
+                console.log("Service worker registration failed, error:"  , err );
+            });
+    }
+}
+
+// Send the registration token your application server, so that it can:
+// - send messages back to this app
+// - subscribe/unsubscribe the token from topics
+function sendTokenToServer(currentToken) {
+    if (!isTokenSentToServer()) {
+        console.log('Sending token to server...');
+        window.localStorage.setItem('token', currentToken);
+        // TODO: Send the current token to your server.
+        setTokenSentToServer(true);
+        if (webhook_url) {
+            $.post(webhook_url, {token: currentToken}, function (data, status) {
+                console.log('Token sent to server...');
+            });
+        }
+
+    } else {
+        console.log('Token already sent to server so won\'t send it again unless it changes');
+    }
+}
+
+// Update local information about token been sent to the server
+function setTokenSentToServer(sent) {
+    window.localStorage.setItem(tokenSentTitle, sent ? '1' : '0');
+}
+
+// Check local information if token was sent to server
+function isTokenSentToServer() {
+    return window.localStorage.getItem(tokenSentTitle) === '1';
+}
